9d5fc3a7944fdbb9cb5c78670e4e9c3442885ef9
Hussein Essam <112550601+Husseinn-Essam@users.noreply.github.com>
2024-12-21 13:03:07 +0200

    debug-mode

--- gesture-recognition/opencvExp.py
+++ gesture-recognition/opencvExp.py
@@ -49,137 +49,155 @@ def handle_key():
         toggle_pause()
 
 
-while True:
-    # Capture the frame twice a second
-    cv2.waitKey(50)
+def gesture_recognition_loop(debug=True,frame=None):
+    while True:
+        # Capture the frame twice a second
 
-    handle_key()
-    
-    if quit:
-        print("Quitting...")
-        break
-    if pause:
-        continue
-
-    ret, frame = cap.read()
-    if not ret:
-        print("Failed to grab frame. Exiting...")
-        break
-    frame = cv2.flip(frame, 1)
-    
-    # Draw the ROI rectangle
-    # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    
-    # Apply image filtering and gesture recognition
-    # roi, thresh, contours = imageFiltering(frame)
-    frame = cv2.GaussianBlur(frame, (5, 5), 0)
-    results = segmenter(frame)
-
-    # At start we may not have a hand in the frame
-    if len(results) == 2:
-        thresh, roi  = results
-        full_frame_segmented = None 
-        capturedFrame = np.zeros_like(frame)  
-    else:
-        thresh, roi,capturedFrame ,full_frame_segmented = results
-    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
-    countoursFull , _ = cv2.findContours(full_frame_segmented, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    drawing = np.zeros(roi.shape, np.uint8)
-    drawing2 = np.zeros(roi.shape, np.uint8)
-    
-
-    try:
-
-        contour = max(contours, key=lambda c: cv2.contourArea(c), default=0)
-        contourFull = max(countoursFull, key=lambda c: cv2.contourArea(c), default=0)
-        palmCenter = get_palm_center(contour)
-        palmCenterFull = get_palm_center(contourFull)
-        cv2.circle(drawing, palmCenter, 5, (0, 0, 255), -1)
-        cv2.circle(full_frame_segmented, palmCenterFull, 5, (0, 0, 255), -1)
-        motion_add_point_to_buffer((palmCenterFull[0], palmCenterFull[1]))
-        hull = cv2.convexHull(contour)
-        cv2.drawContours(drawing, [contour], -1, (0, 255, 0), 1)
-        cv2.drawContours(drawing2, [contourFull], -1, (0, 255, 0), 1)
-        cv2.drawContours(drawing, [hull], -1, (0, 0, 255), 1)
-   
-        hull_indices = cv2.convexHull(contour, returnPoints=False).flatten()
-
-        defects = convexity_defects(contour[:, 0, :], hull_indices)
+        handle_key()
         
-        count_defects = 0
+        if quit:
+            print("Quitting...")
+            break
+        if pause:
+            continue
+        ## in debug mode we function get its frame (self supplied)
+        if(debug == True):
+            cv2.waitKey(50)
+            ret, frame = cap.read()
+            if not ret:
+                print("Failed to grab frame. Exiting...")
+                break
+            frame = cv2.flip(frame, 1)
+    
+        # Draw the ROI rectangle
+        # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         
-        # Filter defects with approximately 90 degrees
-        filtered_defects = []
-        for defect in defects:
-            start_idx, end_idx, far_idx, depth = defect
-            start = tuple(contour[start_idx][0])
-            end = tuple(contour[end_idx][0])
-            far = tuple(contour[far_idx][0])
-            angle = angle_between_points(start, end, far)
-            if  angle < 90:  # Allow a small margin around 90 degrees
-                count_defects += 1
-                filtered_defects.append(defect)
+        # Apply image filtering and gesture recognition
+        # roi, thresh, contours = imageFiltering(frame)
+        frame = cv2.GaussianBlur(frame, (5, 5), 0)
+        results = segmenter(frame)
 
-        # Draw filtered convexity defects
-        for defect in filtered_defects:
-            start_idx, end_idx, far_idx, depth = defect
-            start = tuple(contour[start_idx][0])
-            end = tuple(contour[end_idx][0])
-            far = tuple(contour[far_idx][0])
-            cv2.line(drawing, start, end, (255, 0, 0), 1)  # Blue line for defect
-            cv2.circle(drawing, far, 5, (0, 255, 255), -1)  # Yellow circle for defect point
-        
-        palm_center = get_palm_center(contour)
-        cv2.circle(drawing, palmCenter, 5, (0, 0, 255), -1)
-        direction = detect_pointing_direction(frame, contour)       
-        solidity = calcSolidity(contour)
-        
-        if is_rock_on(contour,drawing,palm_center[0],palm_center[1], count_defects)==True and direction!="Left" and direction!="Right" and solidity<=0.6:
-            gesture = "ROCK ON"
-        elif count_defects == 0:
-            if solidity > 0.6:  # Fist: High solidity (compact shape)
-                gesture = "FIST"
-            else:  # hand with one finger pointing
-                gesture = "ONE"
-            # check direction
-            direction = detect_pointing_direction(frame, contour)
-            cv2.putText(frame, f"Direction: {direction}", (10, 100),
-                                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-        elif count_defects == 1:
-            gesture = "TWO"
-        elif count_defects == 2:
-            gesture = "THREE"
-        elif count_defects == 3:
-            gesture = "FOUR"
-        elif count_defects == 4:
-            gesture = "FIVE"
+        # At start we may not have a hand in the frame
+        if len(results) == 2:
+            thresh, roi  = results
+            full_frame_segmented = None 
+            capturedFrame = np.zeros_like(frame)  
         else:
-            gesture = "UNKNOWN"
+            thresh, roi,capturedFrame ,full_frame_segmented = results
+        contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         
-        motion_detected = motion_track_points()
-        if motion_detected != None:
-            motion_last_detected = motion_detected
+        countoursFull , _ = cv2.findContours(full_frame_segmented, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        drawing = np.zeros(roi.shape, np.uint8)
+        drawing2 = np.zeros(roi.shape, np.uint8)
+        # Initialize gesture and motion variables
+        gesture = "UNKNOWN"
+        motion_detected = "UNKNOWN"
+        direction = "UNKNOWN"
+        motion_last_detected = "UNKNOWN"
+        try:
 
-        cv2.putText(frame, f"Gesture: {gesture}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        cv2.putText(frame, f"Detected Motion: {motion_detected}", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
-        cv2.putText(frame, f"Last Detected Motion: {motion_last_detected}", (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
-
-    except:
-        pass
+            contour = max(contours, key=lambda c: cv2.contourArea(c), default=0)
+            contourFull = max(countoursFull, key=lambda c: cv2.contourArea(c), default=0)
+            palmCenter = get_palm_center(contour)
+            palmCenterFull = get_palm_center(contourFull)
+            cv2.circle(drawing, palmCenter, 5, (0, 0, 255), -1)
+            cv2.circle(full_frame_segmented, palmCenterFull, 5, (0, 0, 255), -1)
+            motion_add_point_to_buffer((palmCenterFull[0], palmCenterFull[1]))
+            hull = cv2.convexHull(contour)
+            cv2.drawContours(drawing, [contour], -1, (0, 255, 0), 1)
+            cv2.drawContours(drawing2, [contourFull], -1, (0, 255, 0), 1)
+            cv2.drawContours(drawing, [hull], -1, (0, 0, 255), 1)
     
-    # display the frames
-    cv2.imshow("ROI", cv2.resize(roi, (300, 400)))
-    cv2.imshow("Threshold", cv2.resize(thresh, (300, 400)))
-    cv2.imshow("Drawing", cv2.resize(drawing, (300, 400)))
-    # cv2.imshow("Drawing2", cv2.resize(drawing2, (300, 400)))
-    cv2.imshow("capturedFrame", capturedFrame)
-    if full_frame_segmented is not None and np.any(full_frame_segmented):
-        cv2.imshow("full_frame_segmented", full_frame_segmented)
-    cv2.imshow("Frame", frame)
-    
-    # motion_handle_roi_buffer_reset()
+            hull_indices = cv2.convexHull(contour, returnPoints=False).flatten()
+
+            defects = convexity_defects(contour[:, 0, :], hull_indices)
+            
+            count_defects = 0
+            
+            # Filter defects with approximately 90 degrees
+            filtered_defects = []
+            for defect in defects:
+                start_idx, end_idx, far_idx, depth = defect
+                start = tuple(contour[start_idx][0])
+                end = tuple(contour[end_idx][0])
+                far = tuple(contour[far_idx][0])
+                angle = angle_between_points(start, end, far)
+                if  angle < 90:  # Allow a small margin around 90 degrees
+                    count_defects += 1
+                    filtered_defects.append(defect)
+
+            # Draw filtered convexity defects
+            for defect in filtered_defects:
+                start_idx, end_idx, far_idx, depth = defect
+                start = tuple(contour[start_idx][0])
+                end = tuple(contour[end_idx][0])
+                far = tuple(contour[far_idx][0])
+                cv2.line(drawing, start, end, (255, 0, 0), 1)  # Blue line for defect
+                cv2.circle(drawing, far, 5, (0, 255, 255), -1)  # Yellow circle for defect point
+            
+            palm_center = get_palm_center(contour)
+            cv2.circle(drawing, palmCenter, 5, (0, 0, 255), -1)
+            direction = detect_pointing_direction(frame, contour)       
+            solidity = calcSolidity(contour)
+            
+            if is_rock_on(contour,drawing,palm_center[0],palm_center[1], count_defects)==True and direction!="Left" and direction!="Right" and solidity<=0.6:
+                gesture = "ROCK ON"
+            elif count_defects == 0:
+                if solidity > 0.6:  # Fist: High solidity (compact shape)
+                    gesture = "FIST"
+                else:  # hand with one finger pointing
+                    gesture = "ONE"
+                # check direction
+                direction = detect_pointing_direction(frame, contour)
+                cv2.putText(frame, f"Direction: {direction}", (10, 100),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+            elif count_defects == 1:
+                gesture = "TWO"
+            elif count_defects == 2:
+                gesture = "THREE"
+            elif count_defects == 3:
+                gesture = "FOUR"
+            elif count_defects == 4:
+                gesture = "FIVE"
+            else:
+                gesture = "UNKNOWN"
+            
+            motion_detected = motion_track_points()
+            if motion_detected != None:
+                motion_last_detected = motion_detected
+             
+            cv2.putText(frame, f"Gesture: {gesture}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(frame, f"Detected Motion: {motion_detected}", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
+            cv2.putText(frame, f"Last Detected Motion: {motion_last_detected}", (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+
+        except:
+            pass
+        # return json that has the gesture and the motion detected and direction
+        # display the frames
+        cv2.imshow("ROI", cv2.resize(roi, (300, 400)))
+        cv2.imshow("Threshold", cv2.resize(thresh, (300, 400)))
+        cv2.imshow("Drawing", cv2.resize(drawing, (300, 400)))
+        # cv2.imshow("Drawing2", cv2.resize(drawing2, (300, 400)))
+        cv2.imshow("capturedFrame", capturedFrame)
+        if full_frame_segmented is not None and np.any(full_frame_segmented):
+            cv2.imshow("full_frame_segmented", full_frame_segmented)
+        cv2.imshow("Frame", frame)
+        
+        # motion_handle_roi_buffer_reset()
+        if(debug == False):
+            
+            result = {}
+            if(gesture):
+                result = {
+                    "gesture": gesture,
+                    "motion_detected": motion_detected,
+                    "direction": direction
+                }
+            return result
 
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+gesture_recognition_loop(debug=True)
